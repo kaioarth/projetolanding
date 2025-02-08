d5b8d14b5b31ead327794a66a17824864a9022ce
kaio artur <kaioacm@gmail.com>
2025-02-07 21:20:51 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function toggleMode() {
+  const html = document.documentElement
+  // if(html.classList.contains('light')) {
+  //  html.classList.remove('light')
+  //  } else {
+  //   html.classList.add('light')
+  //  }
+
+  html.classList.toggle("light")
+
+
+//pegar a tag da img
+const img = document.querySelector("#profile img")
+
+//substituir a img
+if (html.classList.contains("light")) {
+  //se tiver light mode, pegar a light mode
+  img.setAttribute("src", "assets/assets/avatar-light.png")
+} else {
+  //se tiver sem light mode, manter padrão
+  img.setAttribute("src", "./assets/assets/avatar.png")
+}
+}
+
